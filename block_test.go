9f927d8dcddab01806479f2128b218da068e63bd
brandonblock <brandon.block@gmail.com>
2018-08-02 13:10:00 -0500

    test

--- block_test.go
+++ block_test.go
@@ -1,18 +1,19 @@
 package chain
 
 import (
+	"fmt"
 	"testing"
 
 	assert "github.com/stretchr/testify/require"
 )
 
 func TestBlock_SetHash(t *testing.T) {
-	emptyHash := "[95 236 235 102 255 200 111 56 217 82 120 108 109 105 108 121 194 219 194 57 221 78 145 180 103 41 215 58 39 251 87 233]"
 	type fields struct {
 		Timestamp     int64
 		Data          []byte
 		PrevBlockHash []byte
 		Hash          []byte
+		expected      string
 	}
 	tests := []struct {
 		name   string
@@ -25,6 +26,7 @@ func TestBlock_SetHash(t *testing.T) {
 				Data:          []byte{},
 				PrevBlockHash: []byte{},
 				Hash:          []byte{},
+				expected:      "[95 236 235 102 255 200 111 56 217 82 120 108 109 105 108 121 194 219 194 57 221 78 145 180 103 41 215 58 39 251 87 233]",
 			},
 		},
 	}
@@ -37,7 +39,8 @@ func TestBlock_SetHash(t *testing.T) {
 				Hash:          tt.fields.Hash,
 			}
 			b.SetHash()
-			assert.Equal()
+			fmt.Println(b.Hash)
+			assert.Equal(t, tt.fields.expected, fmt.Sprintf("%v", b.Hash), "This hash is deterministic")
 		})
 	}
 }
