67650438d6869afb16022397a26a683f9b5ccd2b
brandonblock <brandon.block@gmail.com>
2018-08-02 13:39:17 -0500

    new blockchain and add block

--- blockchain.go
+++ blockchain.go
@@ -1,5 +1,20 @@
 package chain
 
+// Blockchain holds a historical record of blocks
 type Blockchain struct {
 	blocks []*Block
+}
+
+// NewBlockchain returns a new Blockchain with initial genesis block
+func NewBlockchain() *Blockchain {
+	return &Blockchain{
+		blocks: []*Block{NewGenesisBlock()},
+	}
+}
+
+// AddBlock adds a block to the record
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
 }
