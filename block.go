cd80656c58c6bce82417b1be59e6ce42bbffb034
brandonblock <brandon.block@gmail.com>
2018-08-02 12:51:49 -0500

    SetHash

--- block.go
+++ block.go
@@ -1,9 +1,23 @@
 package chain
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+)
+
 // Block stores on-chain information
 type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+}
+
+// SetHash calculates and sets a SHA 256 hash
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
 }
