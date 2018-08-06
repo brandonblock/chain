457713db89fd50cbb99cb0968aadeb06b8deb751
brandonblock <brandon.block@gmail.com>
2018-08-06 12:23:19 -0500

    Deserialize test

--- block_test.go
+++ block_test.go
@@ -16,10 +16,8 @@ func TestBlock_Serialize(t *testing.T) {
 		Nonce         int
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		want    []byte
-		wantErr bool
+		name   string
+		fields fields
 	}{
 		{
 			name: "no error please",
@@ -27,7 +25,6 @@ func TestBlock_Serialize(t *testing.T) {
 				Timestamp: time.Now().Unix(),
 				Data:      []byte{},
 			},
-			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
@@ -42,6 +39,38 @@ func TestBlock_Serialize(t *testing.T) {
 			got, err := b.Serialize()
 			assert.NoError(t, err, "this should be error-free")
 			assert.NotNil(t, got, "if there's no error, this shouldn't be nil")
+		})
+	}
+}
+
+func TestDeserializeBlock(t *testing.T) {
+
+	b := &Block{
+		Timestamp: time.Now().Unix(),
+		Data:      []byte{},
+	}
+
+	encoded, _ := b.Serialize()
+
+	type args struct {
+		d []byte
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "success",
+			args: args{
+				d: encoded,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := DeserializeBlock(tt.args.d)
+			assert.NoError(t, err, "this should be error-free")
+			assert.IsType(t, &Block{}, got, "return should be of type *Block")
 		})
 	}
 }
