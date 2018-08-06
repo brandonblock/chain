9404869d150e5ce7891614896469eeab50bffa53
brandonblock <brandon.block@gmail.com>
2018-08-06 13:35:15 -0500

    rewrite tests & benchmark

--- blockchain_test.go
+++ blockchain_test.go
@@ -6,19 +6,6 @@ import (
 	assert "github.com/stretchr/testify/require"
 )
 
-// func TestBlockchain_AddBlock(t *testing.T) {
-// 	bc, _ := NewBlockchain()
-// assert.NoError(t, err, "blockchain creation should succeed")
-
-// bc.AddBlock("send 1 coin to someone")
-// bc.AddBlock("send 1 coin to someone else")
-// TODO: re-write this test to read from db
-// 	for _, block := range bc.blocks {
-// 		p := NewProofOfWork(block)
-// 		assert.True(t, p.Validate(), "validation should be true")
-// 	}
-// }s
-
 func TestNewBlockchain(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -35,5 +22,27 @@ func TestNewBlockchain(t *testing.T) {
 			assert.NoError(t, err, "no error")
 			assert.IsType(t, &Blockchain{}, got, "if there's no error, this should be of type *Blockchain")
 		})
+	}
+}
+
+func TestBlockchain_AddBlock(t *testing.T) {
+	bc, _ := NewBlockchain()
+
+	err := bc.AddBlock("send 1 coin to someone")
+	assert.NoError(t, err, "this should persist a transaction")
+	err = bc.AddBlock("send 1 coin to someone else")
+	assert.NoError(t, err, "this should persist another transaction")
+
+	// TODO: re-write this test to read from db
+	// for _, block := range bc.blocks {
+	// 	p := NewProofOfWork(block)
+	// 	assert.True(t, p.Validate(), "validation should be true")
+	// }
+}
+
+func BenchmarkBlockchain_AddBlock(b *testing.B) {
+	bc, _ := NewBlockchain()
+	for n := 0; n < b.N; n++ {
+		bc.AddBlock("send 1 coin to someone")
 	}
 }
