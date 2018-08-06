5f4afe093e428bd350e0e0e22c9869cdfc601f77
brandonblock <brandon.block@gmail.com>
2018-08-06 12:15:25 -0500

    serialize test

--- block_test.go
+++ block_test.go
@@ -1,1 +1,47 @@
 package chain
+
+import (
+	"testing"
+	"time"
+
+	assert "github.com/stretchr/testify/require"
+)
+
+func TestBlock_Serialize(t *testing.T) {
+	type fields struct {
+		Timestamp     int64
+		Data          []byte
+		PrevBlockHash []byte
+		Hash          []byte
+		Nonce         int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name: "no error please",
+			fields: fields{
+				Timestamp: time.Now().Unix(),
+				Data:      []byte{},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &Block{
+				Timestamp:     tt.fields.Timestamp,
+				Data:          tt.fields.Data,
+				PrevBlockHash: tt.fields.PrevBlockHash,
+				Hash:          tt.fields.Hash,
+				Nonce:         tt.fields.Nonce,
+			}
+			got, err := b.Serialize()
+			assert.NoError(t, err, "this should be error-free")
+			assert.NotNil(t, got, "if there's no error, this shouldn't be nil")
+		})
+	}
+}
