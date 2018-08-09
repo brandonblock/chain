da576dbc2cbc86f2f122e77170151a00e38d8791
brandonblock <brandon.block@gmail.com>
2018-08-08 19:12:51 -0500

    setID and outgoing TX

--- transaction.go
+++ transaction.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"encoding/hex"
+	"fmt"
+)
 
 // Transaction is a combination of inputs and outputs
 type Transaction struct {
@@ -15,6 +21,21 @@ const subsidy int = 210000
 //IsCoinbase returns whether or not this is the coinbase transaction
 func (t *Transaction) IsCoinbase() bool {
 	return len(t.Vin) == 1 && len(t.Vin[0].Txid) == 0 && t.Vin[0].Vout == -1
+}
+
+// SetID sets ID of a transaction
+func (t *Transaction) SetID() error {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(t)
+	if err != nil {
+		return err
+	}
+	hash = sha256.Sum256(encoded.Bytes())
+	t.ID = hash[:]
+	return nil
 }
 
 // TXInput is the first part of a transaction
@@ -69,4 +90,39 @@ func NewCoinbaseTX(to, data string) *Transaction {
 	}
 
 	return &tx
+}
+
+// NewUTXOTransaction creates a transaction between addresses
+func NewUTXOTransaction(from, to string, amount int, bc *Blockchain) (*Transaction, error) {
+	var inputs []TXInput
+	var outputs []TXOutput
+
+	acc, validOutputs, err := bc.FindSpendableOutputs(from, amount)
+	if err != nil {
+		return nil, err
+	}
+
+	if acc < amount {
+		return nil, fmt.Errorf("Not enough funds")
+	}
+
+	// List inputs
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, out := range outs {
+			input := TXInput{txID, out, from}
+			inputs = append(inputs, input)
+		}
+
+		//List outputs
+		outputs = append(outputs, TXOutput{acc - amount, from})
+	}
+
+	tx := Transaction{nil, inputs, outputs}
+
+	return &tx, tx.SetID()
 }
